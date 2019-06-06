3d7fa24f27b552677a124c6ed478e4c7f8583803
kunjesh1 <kumarkunjesh1@gmail.com>
2019-06-06 12:09:14 +0530

    Add the server component
    Added the server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-
+import { ServerComponent } from "./server/server.component";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
