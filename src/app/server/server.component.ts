3caeab80eafef4a38afcb7d6789219b4328825ce
kunjesh1 <kumarkunjesh1@gmail.com>
2019-06-06 17:20:45 +0530

    Dynamic server status

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,14 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-server",
-  templateUrl: "./server.component.html"
+  templateUrl: "./server.component.html",
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `
+  ]
 })
 export class ServerComponent {
   serverId: number = 10;
@@ -14,5 +21,9 @@ export class ServerComponent {
 
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === "online" ? "green" : "red";
   }
 }
