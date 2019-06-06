5f4497fc6a05b41e0cfb36658b9e90ecbf56b4fa
kunjesh1 <kumarkunjesh1@gmail.com>
2019-06-06 12:16:18 +0530

    Added the second component
    Added the second component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { ServerComponent } from "./server/server.component";
+import { ServersComponent } from "./servers/servers.component";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
